35279aca54b3ff0af7a4874043761bb112a48f88
schniti269 <destinator28@gmail.com>
2023-08-29 21:02:11 +0200

    added test

--- Methods/tagging.py
+++ Methods/tagging.py
@@ -1,48 +1,52 @@
 import spacy
-
+import pandas as pd
 
 nlp_en = spacy.load("en_core_web_sm")
 nlp_de = spacy.load("de_core_news_sm")
 confidence_threshold = 0.2
 
-tags = ["NNP", "NN"]
+tags = ["NNP", "NN","NNS"]
 forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj"]
 
 def add_hashtags(text):
     doc_en = nlp_en(text)
     doc_de= nlp_de(text)
     
-    Topics = set()
-    for token in [doc_en,doc_de]:
-        if token.dep_ in forbidden_dep:
-            continue
-        if token=="-":
-            continue
-        if token.tag_ in tags:
-            Topics.append(f"#{token.lemma_}")
-    
-    return text+"\n"+"\n   ".join(Topics)
+    topics = set()
+    for doc in [doc_en, doc_de]:
+        for token in doc:
+            if token.dep_ in forbidden_dep:
+                continue
+            if token.text == "-":
+                continue
+            if token.tag_ in tags:
+                topics.add(f"#{token.lemma_}")
+
+    return text + "\n" + "\n   ".join(topics)
 
 def create_table(sentence):
-    hashtags = add_hashtags(sentence)
     doc = nlp_en(sentence)  # Default to English for token information
 
-    if hashtags:
-        detector = Detector(sentence, quiet=True)
-        lang = detector.language.code
-
-        if lang == "de":
-            doc = nlp_de(sentence)
+    doc = nlp_de(sentence)
     
-    table = f"{doc.lang.capitalize()}:\n"
-    table += "|   TEXT   |   LEMMA   |  POS  |   DEP   |  HASH   |\n"
-    table += "|:--------:|:---------:|:-----:|:-------:|:-------:|\n"
+    data = {
+        "TEXT": [],
+        "LEMMA": [],
+        "POS": [],
+        "TAG": [],
+        "DEP": [],
+    }
     
     for token in doc:
-        if token.text in hashtags:
-            table += f"|  {token.text}  |  {token.lemma_}  |  {token.pos_}  |  {token.dep_}  |  {hashtags[hashtags.index(token.text)]}  |\n"
+
+        data["TEXT"].append(token.text)
+        data["LEMMA"].append(token.lemma_)
+        data["POS"].append(token.pos_)
+        data["TAG"].append(token.tag_)
+        data["DEP"].append(token.dep_)
     
-    return table
+    df = pd.DataFrame(data)
+    return df
 
 if __name__ == "__main__":
     sentence = "I like trains because trains are cool."
