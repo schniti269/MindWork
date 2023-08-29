ded6989991d024cd575de0d37a07c51b6bc0c423
schniti269 <destinator28@gmail.com>
2023-08-29 23:02:50 +0200

    new calibrations

--- Methods/PDFToTaggedMD.py
+++ Methods/PDFToTaggedMD.py
@@ -14,17 +14,36 @@ def extract_text_from_pdf(pdf_path):
 def process_pdf(pdf_path, output_folder):
     try:
         base_name = os.path.basename(pdf_path)
+        base_name_without_extension = os.path.splitext(base_name)[0]
         
         print(f"Processing {base_name}...")
         
         text = extract_text_from_pdf(pdf_path)
-        tagged_text = add_hashtags(text)
         
-        # Save tagged text to an MD file
-        output_file = os.path.join(output_folder, f"{base_name}.md")
-        with open(output_file, "w", encoding="utf-8") as f:
-            f.write(tagged_text)
-            print(f"Saved {output_file}")
+        # Split text into segments
+        segments = []
+        current_segment = ""
+        lines = text.split('\n')
+        for line in lines:
+            if line.strip() == "" or len(current_segment.split('\n')) >= 6:
+                if current_segment.strip() != "":
+                    segments.append(current_segment)
+                current_segment = ""
+            current_segment += line + '\n'
+        if current_segment.strip() != "":
+            segments.append(current_segment)
+        
+        # Create a folder for the PDF
+        pdf_folder = os.path.join(output_folder, "Scripts", base_name_without_extension)
+        os.makedirs(pdf_folder, exist_ok=True)
+        
+        # Save segments to individual MD files with hashtags
+        for idx, segment in enumerate(segments):
+            tagged_segment = add_hashtags(segment)
+            output_file = os.path.join(pdf_folder, f"{base_name_without_extension}_{idx + 1}.md")
+            with open(output_file, "w", encoding="utf-8") as f:
+                f.write(tagged_segment)
+                print(f"Saved {output_file}")
     except Exception as e:
         print(f"Error processing {pdf_path}: {e}")
 
@@ -40,6 +59,6 @@ def main(input_folder, output_folder):
         print(f"Processed {pdf_file}")
 
 if __name__ == "__main__":
-    input_folder = r"C:\Users\ian-s\Desktop\Mindwork\Scripts"
-    output_folder = r"C:\Users\ian-s\Desktop\Mindwork\Output"
+    input_folder = r"C:\Users\ian-s\Repos\MindWork\Scripts"
+    output_folder = r"C:\Users\ian-s\Repos\MindWork\Output"
     main(input_folder, output_folder)
--- Methods/tagging.py
+++ Methods/tagging.py
@@ -5,37 +5,36 @@ nlp_en = spacy.load("en_core_web_sm")
 nlp_de = spacy.load("de_core_news_sm")
 confidence_threshold = 0.2
 
+forbidden_token = set()
+tags = ["NNP", "NN", "NNS","NP"]
+
+forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj", "punct", "pnc", "nk"]
+forbidden_tag = ["KO", "ART", "NE", "ADJA", "XY", "ADJ", "AUX", "CONJ", "PRON", "ADP", "ADV", "DET", "INTJ", "NUM", "PART"]
 
 def add_hashtags(text):
-    tags = ["NNP", "NN","NNS","TRUNK"]
-
-    forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj","punct","pnc","nk"]
-    forbidden_tag = ["ART","NE","ADJA","XY"]
-
-    forbidden_token =set()
-    topics = set()
+    current_topics = set()
 
     doc_en = nlp_en(text)
-    doc_de= nlp_de(text)
+    doc_de = nlp_de(text)
 
     for doc in [doc_de, doc_en]:
         for token in doc:
             if token.text in forbidden_token:
                 continue
-            if token.tag_  in forbidden_tag or token.dep_ in forbidden_dep:
-                print("detected "+token.text+" as"+token.tag_+" "+token.dep_)
+            if token.tag_ in forbidden_tag or token.dep_ in forbidden_dep:
+                print(f"detected {token.text} as {token.tag_} {token.dep_}")
                 forbidden_token.add(token.text)
                 continue
             if token.tag_ in tags:
-                topics.add(f"#{token.lemma_}")
-
-    return text + "\n" + "\n   ".join(topics)
+                current_topics.add(f"#{token.lemma_}")
+    current_topics -= forbidden_token  # Remove forbidden tokens from current_topics
+    return text + "\n" + "\n   ".join(current_topics)
 
 def create_table(sentence):
+    # Note: The following two lines of code overwrite the same variable 'doc'
     doc = nlp_en(sentence)  # Default to English for token information
-
     doc = nlp_de(sentence)
-    
+
     data = {
         "TEXT": [],
         "LEMMA": [],
@@ -43,21 +42,19 @@ def create_table(sentence):
         "TAG": [],
         "DEP": [],
     }
-    
-    for token in doc:
 
+    for token in doc:
         data["TEXT"].append(token.text)
         data["LEMMA"].append(token.lemma_)
         data["POS"].append(token.pos_)
         data["TAG"].append(token.tag_)
         data["DEP"].append(token.dep_)
-    
+
     df = pd.DataFrame(data)
     return df
 
 if __name__ == "__main__":
     sentence = "### Produktionsseite- Wirtschaftlichkeit- einsatzsteuerung der Produktionsfaktoren -  Gegeben = Input Menge- Maximaler Output- gegeben= Output Menge- Ininmaler Input."
-    
     print(add_hashtags(sentence))
     table = create_table(sentence)
     print(table)
