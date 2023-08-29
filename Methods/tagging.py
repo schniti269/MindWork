fd73c370fd381918c3ef23dcf9e713f8e51c3ff7
schniti269 <destinator28@gmail.com>
2023-08-29 21:36:57 +0200

    adjusted

--- Methods/tagging.py
+++ Methods/tagging.py
@@ -5,19 +5,26 @@ nlp_en = spacy.load("en_core_web_sm")
 nlp_de = spacy.load("de_core_news_sm")
 confidence_threshold = 0.2
 
-tags = ["NNP", "NN","NNS"]
-forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj"]
 
 def add_hashtags(text):
+    tags = ["NNP", "NN","NNS","TRUNK"]
+
+    forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj","punct","pnc","nk"]
+    forbidden_tag = ["ART","NE","ADJA","XY"]
+
+    forbidden_token =set()
+    topics = set()
+
     doc_en = nlp_en(text)
     doc_de= nlp_de(text)
-    
-    topics = set()
-    for doc in [doc_en, doc_de]:
+
+    for doc in [doc_de, doc_en]:
         for token in doc:
-            if token.dep_ in forbidden_dep:
+            if token.text in forbidden_token:
                 continue
-            if token.text == "-":
+            if token.tag_  in forbidden_tag or token.dep_ in forbidden_dep:
+                print("detected "+token.text+" as"+token.tag_+" "+token.dep_)
+                forbidden_token.add(token.text)
                 continue
             if token.tag_ in tags:
                 topics.add(f"#{token.lemma_}")
@@ -49,7 +56,8 @@ def create_table(sentence):
     return df
 
 if __name__ == "__main__":
-    sentence = "I like trains because trains are cool."
+    sentence = "### Produktionsseite- Wirtschaftlichkeit- einsatzsteuerung der Produktionsfaktoren -  Gegeben = Input Menge- Maximaler Output- gegeben= Output Menge- Ininmaler Input."
+    
     print(add_hashtags(sentence))
     table = create_table(sentence)
     print(table)
