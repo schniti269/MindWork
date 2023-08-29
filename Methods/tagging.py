ad08186b9eba78e2b6ae930827a5b8df1e6f2cba
schniti269 <destinator28@gmail.com>
2023-08-29 20:13:54 +0200

    Restruct

--- Methods/tagging.py
+++ Methods/tagging.py
@@ -1,5 +1,5 @@
 import spacy
-from polyglot.detect import Detector
+
 
 nlp_en = spacy.load("en_core_web_sm")
 nlp_de = spacy.load("de_core_news_sm")
@@ -9,29 +9,19 @@ tags = ["NNP", "NN"]
 forbidden_dep = ["aux", "prep", "pcomp", "dobj", "pobj"]
 
 def add_hashtags(text):
-    detector = Detector(text, quiet=True)
-    lang = detector.language.code
-
-    if lang == "en":
-        doc = nlp_en(text)
-    elif lang == "de":
-        doc = nlp_de(text)
-    else:
-        return text
+    doc_en = nlp_en(text)
+    doc_de= nlp_de(text)
     
-    new_tokens = []
-    for token in doc:
+    Topics = set()
+    for token in [doc_en,doc_de]:
         if token.dep_ in forbidden_dep:
             continue
-        
-        # Disregard auxiliary verbs (AUX) and only process nouns (NNP, NN)
-        if token.pos_ == "AUX":
+        if token=="-":
             continue
-        
         if token.tag_ in tags:
-            new_tokens.append(f"#{token.lemma_}")
+            Topics.append(f"#{token.lemma_}")
     
-    return new_tokens
+    return text+"\n"+"\n   ".join(Topics)
 
 def create_table(sentence):
     hashtags = add_hashtags(sentence)
@@ -56,5 +46,6 @@ def create_table(sentence):
 
 if __name__ == "__main__":
     sentence = "I like trains because trains are cool."
+    print(add_hashtags(sentence))
     table = create_table(sentence)
     print(table)
